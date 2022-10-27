4c342fa8aa1f41f70ee9da2fd2fd558a71b12b6d
Kendra Yoshizawa <kendrayoshizawa@Kendras-MacBook-Air.local>
2022-10-27 15:26:52 -0700

    troubleshooting user info

--- controllers/thinkers.js
+++ controllers/thinkers.js
@@ -11,23 +11,23 @@ function index(req, res, next) {
   res.render("thoughts/index", { user: req.user });
   // Make the query object to use with Thinker.find based up
   // the user has submitted the search form or now
-  // let modelQuery = req.query.name
-  //   ? { name: new RegExp(req.query.name, "i") }
-  //   : {};
-  // // Default to sorting by name
-  // let sortKey = req.query.sort || "name";
-  // Thinker.find(modelQuery)
-  //   .sort(sortKey)
-  //   .exec(function (err, thinkers) {
-  //     if (err) return next(err);
-  //     // Passing search values, name & sortKey, for use in the EJS
-  //     res.render("thinkers/index", {
-  //       thinkers,
-  //       name: req.query.name,
-  //       sortKey,
-  //       user: req.user,
-  //     });
-  //   });
+  let modelQuery = req.query.name
+    ? { name: new RegExp(req.query.name, "i") }
+    : {};
+  // Default to sorting by name
+  let sortKey = req.query.sort || "name";
+  Thinker.find(modelQuery)
+    .sort(sortKey)
+    .exec(function (err, thinkers) {
+      if (err) return next(err);
+      // Passing search values, name & sortKey, for use in the EJS
+      res.render("thinkers/index", {
+        thinkers,
+        name: req.query.name,
+        sortKey,
+        user: req.user,
+      });
+    });
 }
 
 // function addFact(req, res, next) {
